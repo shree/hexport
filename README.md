6d3da3d4213ea4f240c658994fe052ca895172e6
shree <srichekster@gmail.com>
2017-04-11 22:29:58 -0700

    dun

---
+++ README.md
@@ -0,0 +1,99 @@
+Hexport-test
+=============
+
+**Hexport-test** is the backend focused take home interview project for Hexport Engineering.
+
+Given a database with already seeded tables and data, please create a database-backed web application server that responds to a URL and returns json. More specifically, your web server will respond to the `GET index` endpoint `/api/v1/shipments` and return data according to the specification as described in `api/v1/shipments_spec.rb`
+
+The Hexport backend is written in Ruby on Rails, and while its influence is obvious, this project is designed to be language and framework agnostic. Your web server can be written in Python with Django/Flask, Javascript with Node+Express, or something more exotic. The tests are written in rspec+Ruby, but should be very readable even if you don't know any Ruby: All they do is ping a URL and inspect the json response.
+
+Instructions
+------------
+
+1. Read the rest of this README and review `api/v1/shipments_spec.rb` to understand the endpoint requirements
+2. Create your sample application. Instead of a traditional database, your database will be a set of CSV files.
+3. Perform the following one time setup steps to get the spec runner working
+  1. Navigate to the project root
+  2. Modify `spec/config.rb` if necessary
+  3. Install ruby if necessary
+  4. Install the `bundler` gem if necessary (ex. gem install bundler)
+  5. Run `bundle install` (similar to npm install)
+  6. Make sure your application server is running
+4. Run `bundle exec rspec` until specs pass
+
+What we're looking for
+----------------------
+
+We'd like you to strike a balance between maintainability and speed, with a mild preference towards maintainability. (After all, we've got to read this code to judge it)
+
+Don't worry too much about where it falls in the spectrum though; it's more important that when we talk about your code that you recognize the tradeoffs you made and what you can cut/add if asked to move in either direction.
+
+In particular, if there's a (well respected) library or framework that you would like to use as part of your implementation, please use it. We're here to make working software that accommodates (some frankly insane) business logic, not to reimplement bcrypt.
+
+The rspec test
+--------------
+
+After resetting the database, the [rspec test](https://github.com/horizons-school-of-technology/hexport/blob/master/spec/api/v1/shipments_spec.rb) pings `GET /api/v1/shipments` with various parameters and examines the json response. The spec can be split out five sections:
+- Examining the contents of the json for a single record
+- Sorting
+- Filtering
+- Pagination
+- Error Handling
+
+Hint: For Pagination - instead of using another database, just create your own CSV file
+
+The desired output as defined in the "contents of a single record" section deliberately contains some questionable implementation choices. Please accommodate the desired output and we can discuss the pros and cons of the given json structure.
+
+The Database and Schema
+-----------------------
+
+The sample csv files provided consists of four tables:
+- companies
+- shipments
+- products
+- shipment_products
+
+Both shipments and products have a `company_id` (belong to a company).
+
+The shipment_products table is a join table that connects shipments and products, and thus has both a `product_id` and `company_id`.
+
+```
+Table "companies"
+   Column   |            Type
+------------+-----------------------------
+ id         | integer
+ name       | character varying
+ created_at | timestamp without time zone
+ updated_at | timestamp without time zone
+
+Table "shipments"
+              Column               |            Type
+-----------------------------------+-----------------------------
+ id                                | integer
+ name                              | character varying
+ company_id                        | integer
+ international_transportation_mode | character varying
+ international_departure_date      | date
+ created_at                        | timestamp without time zone
+ updated_at                        | timestamp without time zone
+
+Table "products"
+   Column    |            Type
+-------------+-----------------------------
+ id          | integer
+ sku         | character varying
+ description | character varying
+ company_id  | integer
+ created_at  | timestamp without time zone
+ updated_at  | timestamp without time zone
+
+Table "shipment_products"
+   Column    |            Type
+-------------+-----------------------------
+ id          | integer
+ product_id  | integer
+ shipment_id | integer
+ quantity    | integer
+ created_at  | timestamp without time zone
+ updated_at  | timestamp without time zone
+```
---
+++ index.js
@@ -0,0 +1,184 @@
+var express = require('express');
+var csvFilePath ='./db/shipments.csv';
+var csv = require('csvtojson');
+var app = express();
+
+var shipmentData ='./db/shipments.csv';
+var companyData ='./db/companies.csv';
+var productData ='./db/products.csv';
+var shipmentProductsData ='./db/shipment_products.csv';
+
+app.get('/api/v1/shipments', (req,res)=>{
+  // read from csv and make json
+
+  if(!req.query.company_id){
+    res.status(422).json({
+      errors: ["company_id is required"]
+    });
+    res.send("LOLOLOLOL");
+  }
+
+  var shipments = [];
+  var companies = [];
+  var products = [];
+  var shipmentProducts = [];
+  var company_id = req.query.company_id;
+  var sort = req.query.sort;
+  var direction = req.query.direction;
+  var mode = req.query.international_transportation_mode;
+
+  //Get shipment Data
+  csv().fromFile(shipmentData).on('json',(ship) => {
+    shipments.push(ship);
+  }).on('done',(error)=>{
+    //Get company Data
+    csv().fromFile(companyData).on('json',(com) => {
+      companies.push(com);
+    }).on('done',(error)=>{
+      //Get product Data
+      csv().fromFile(productData).on('json',(produ) => {
+        products.push(produ);
+      }).on('done',(error)=>{
+        //Get shipmentProduct Information
+        csv().fromFile(shipmentProductsData).on('json',(jsonObj) => {
+          shipmentProducts.push(jsonObj);
+        }).on('done',(error)=>{
+          //Test case 1
+          var recordsArray = [];
+          for(var i = 0; i < shipments.length; i++){
+            if(shipments[i].company_id === company_id){
+              shipments[i].products = [];
+              recordsArray.push(shipments[i])
+            }
+          }
+
+          var shipPro = [];
+          for(var j = 0; j < recordsArray.length; j++){
+            for(var k = 0; k < shipmentProducts.length; k++){
+              if(recordsArray[j].id === shipmentProducts[k].shipment_id){
+                shipPro.push(shipmentProducts[k]);
+              }
+            };
+          };
+
+          // console.log(shipPro);
+
+          var myProds = [];
+          for(var l = 0; l < shipPro.length; l++){
+            for(var m = 0; m < products.length; m++){
+              if(shipPro[l].product_id === products[m].id){
+                products[m].quantity = shipPro[l].quantity;
+                products[m].shipment_id = shipPro[l].shipment_id;
+                myProds.push(Object.assign({}, products[m]));
+              }
+            }
+          }
+
+          //set active shipment count
+          // var fml = [];
+          for(var p = 0; p < myProds.length; p++){
+            myProds[p].active_shipment_count = 0;
+            for(var q = 0; q < myProds.length; q++){
+              if(myProds[p].id === myProds[q].id){
+                myProds[p].active_shipment_count++;
+              }
+            }
+            // fml.push(myProds[p])
+            // fml.push(Object.assign({}, myProds[p]));
+          }
+
+          //For each time a product appears increment active count
+          for(var n = 0; n < myProds.length; n++){
+            for(var o = 0; o < recordsArray.length; o++){
+              if(myProds[n].shipment_id === recordsArray[o].id){
+                recordsArray[o].products.push(myProds[n]);
+              }
+            }
+          }
+
+          //Make Id into integers
+
+          for(var lo = 0; lo < recordsArray.length; lo++){
+            recordsArray[lo].id = parseInt(recordsArray[lo].id);
+          }
+          // recordsArray.sort((a,b)=>{
+          //   return a.id > b.id;
+          // });
+
+          if(sort === 'international_departure_date'){
+            if(direction === 'asc'){
+              recordsArray.sort(function(a,b){
+                return Date.parse(a.international_department_date)>Date.parse(b.international_department_date)
+              });
+            }else{
+              recordsArray.sort(function(a,b){
+                return Date.parse(a.international_department_date)<Date.parse(b.international_department_date)
+              });
+            }
+          }
+
+          if(mode === 'ocean'){
+            recordsArray = recordsArray.filter((item) => {
+              return item.international_transportation_mode === 'ocean'
+            })
+          }else if(mode === 'truck'){
+            recordsArray = recordsArray.filter((item) => {
+              return item.international_transportation_mode === 'truck'
+            })
+          }
+
+
+          var solution = []
+          if(!req.query.page){
+            solution = recordsArray.slice(0,4);
+          }else{
+            if(req.query.per){
+              solution = renderPagination(recordsArray,parseInt(req.query.page),parseInt(req.query.per));
+            }else{
+              solution = renderPagination(recordsArray,parseInt(req.query.page),4);
+            }
+          }
+
+
+
+          // console.log(myProds);
+          //console.log(shipPro.length);
+          // console.log(recordsArray);
+          res.json({
+            records: solution
+          });
+
+        });
+      });
+    });
+  });
+});
+
+function renderPagination(temp,pageno, max_num) {
+    return temp.slice( (pageno - 1) * max_num, pageno * max_num );
+}
+
+
+//TEST CASE 1 PASSES
+// app.get('/api/v1/shipments', (req,res)=>{
+//   // read from csv and make json
+//   var myResults = [];
+//   var compArray = [];
+//   var company_id = req.query.company_id;
+//
+//   csv().fromFile(csvFilePath).on('json',(jsonObj) => {
+//     myResults.push(jsonObj);
+//   }).on('done',(error)=>{
+//     for(var i = 0; i < myResults.length; i++){
+//       if(company_id === myResults[i].company_id){
+//         compArray.push(myResults[i])
+//       }
+//     }
+//     console.log(compArray);
+//     res.json({
+//       records: compArray
+//     });
+//   });
+// });
+
+app.listen(3000);
